73a09de5e1ed41499457887b14bfb050164093d9
Elijah Lopez <elijahllopezz@gmail.com>
2019-03-02 21:33:53 -0500

    Initial commit

---
+++ zip.py
@@ -0,0 +1,40 @@
+from zipfile import ZipFile
+import os
+import json
+
+
+with open('manifest.json') as f:
+    data = json.load(f)
+
+version = data['version'].split('.')
+version[3] = str(1 + int(version[3]))
+version = '.'.join(version)
+data['version'] = version
+
+with open('manifest.json', 'w') as fp:
+    json.dump(data, fp)
+
+name = data['short_name']
+filename = name + ' ' + version + '.zip'
+
+if not os.path.exists('Builds'):
+    os.makedirs('Builds')
+
+try:
+    with ZipFile('Builds/' + filename, 'w') as zf:
+        zf.write('manifest.json')  # add manifiest.json to archive
+
+        # add icons to archive
+        # for icon in os.listdir('icons'):
+        #     zf.write(f'icons/{icon}')
+
+        # add css file to archive
+        zf.write('style.css')
+    done = True
+except Exception as e:
+    if e != KeyboardInterrupt:
+        print(e)
+        breakpoint()
+
+print('Zip successful')
+# TODO: use web-ext
