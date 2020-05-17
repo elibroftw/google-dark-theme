0156efdd4d9325a42f21d4dc3064278950e834a0
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-16 22:42:18 -0400

    Updated style.css

--- build.py
+++ build.py
@@ -57,6 +57,6 @@ URL_NAME = 'dark-theme-for-google-searches'
 # GUID = '000a8ba3-ef46-40fd-a51c-daf19e7c00e7'
 # requests.put(f'https://addons.mozilla.org/api/v4/addons/{GUID}/versions/{version}/')
 
-webbrowser.open('https://userstyles.org/styles/180957/edit')
-webbrowser.open(f'https://addons.mozilla.org/en-CA/developers/addon/{URL_NAME}/versions/submit/')
-webbrowser.open('https://chrome.google.com/webstore/devconsole/d9cb1dfc-39c3-47c1-83ca-1ec7b4652439/ohhpliipfhicocldcakcgpbbcmkjkian/edit/package')
+print('https://userstyles.org/styles/180957/edit')
+print(f'https://addons.mozilla.org/en-CA/developers/addon/{URL_NAME}/versions/submit/')
+print('https://chrome.google.com/webstore/devconsole/d9cb1dfc-39c3-47c1-83ca-1ec7b4652439/ohhpliipfhicocldcakcgpbbcmkjkian/edit/package')
